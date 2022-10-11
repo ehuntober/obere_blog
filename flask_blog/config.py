719f63224b492003e4ed7e4c0993d5340bb87404
ehuntober <ehuntercrypto@gmail.com>
2022-10-11 11:16:57 +0000

    flask_blog modified

---
+++ flask_blog/config.py
@@ -0,0 +1,10 @@
+from distutils.debug import DEBUG
+
+
+DEBUG=True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+USERNAME = 'ehuntober'
+PASSWORD = 'win$30.'
+SECRET_KEY='16da09419c7fef7b82ec'
+
