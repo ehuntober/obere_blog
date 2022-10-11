cb1630d566b8a325bb8f1cbadfef479620baa712
ehuntober <ehuntercrypto@gmail.com>
2022-10-11 13:37:07 +0000

    showing detail page added

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -33,6 +33,13 @@ def add_entry():
     flash('A new article has been created.')
     return redirect(url_for('show_entries'))
 
+@app.route('/entries/<int:id>', methods=['GET'])
+def show_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    return render_template('entries/show.html', entry=entry)
+
 
 @app.route('/login',methods=['GET','POST'])
 def login():
